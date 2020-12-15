12363af9a6b1ba8f3c733fb041b60cb45bef28e6
pascalculate <high.dragonborn@gmail.com>
2020-12-15 20:09:24 +0700

    khoa up cac file duoi py

--- manage.py
+++ manage.py
@@ -5,8 +5,7 @@ import sys
 
 
 def main():
-    """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_app.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'OCRDjangoApi.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
